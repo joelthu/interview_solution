96e1b033b8abfb6e23d60bff29501c6f48a2faa0
Joel Hughes <joelhughes06@netscape.net>
2015-09-15 22:45:43 -0600

    0915152245

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,11 +15,30 @@ describe User do
 end
 
 describe UsersController, :type => :controller do
+
   describe "GET index" do
     it "returns a 200" do
       get :index, format: :json
       response.should be_successful
     end   
   end
+
+  describe "GET show" do
+    let(:user) { User.create(id: 1, first_name: 'Abe', last_name: 'Atkins', email: 'aatkins@gmail.com', social_security_number: '111223333') }    
+    it "returns a 200 for show" do
+      get :show, :id => user.id, format: :json
+      response.should be_successful
+    end   
+  end
+
 end
 
+describe User do
+
+  let(:user) { User.create(id: 1, first_name: 'Abe', last_name: 'Atkins', email: 'aatkins@gmail.com') }
+
+  it "does not return a valid user upon create request" do
+    user.should_not be_valid
+  end
+
+end
